cf231946570f7b07b60480ce92a36969f7f338e1
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-03-21 14:18:10 +0600

    ES6-Classes | this | new | subclass | instance | extends | inheritance | super()

--- OOP-P1.js
+++ OOP-P1.js
@@ -1,16 +1,25 @@
-function NBAplayer(name, team, height, age, signatureScoringStyle) {
-  this.name = name;
-  this.team = team;
-  this.height = height;
-  this.age = age;
-  this.signatureScoringStyle = signatureScoringStyle;
+class NBAplayer {
+  constructor(name, team, height, age, signatureScoringStyle) {
+    this.name = name;
+    this.team = team;
+    this.height = height;
+    this.age = age;
+    this.signatureScoringStyle = signatureScoringStyle;
+  }
+  score() {
+    return `${this.name} scores with a ${this.signatureScoringStyle}`;
+  }
 }
 
-NBAplayer.prototype.score = function () {
-  return `${this.name} scores with a ${this.signatureScoringStyle}`;
-};
+/*****************************************************************************/
 
-const lebronJames = new NBAplayer("Lebron James", "Los Angeles Lakers", "6 ft 8 in", 37, "tomahawk dunk");
+class LebronJames extends NBAplayer {
+  block() {
+    return `${this.name} blocks the shot with a chase down block`;
+  }
+}
+
+const lebronJames = new LebronJames("Lebron James", "Los Angeles Lakers", "6 ft 8 in", 37, "tomahawk dunk");
 
 console.log(lebronJames);
 console.log(lebronJames.name);
@@ -19,6 +28,9 @@ console.log(lebronJames.height);
 console.log(lebronJames.age);
 console.log(lebronJames.signatureScoringStyle);
 console.log(lebronJames.score());
+console.log(lebronJames.block());
+
+/*****************************************************************************/
 
 const kevinDurant = new NBAplayer("Kevin Durant", "Brooklyn Nets", "6 ft 10 in", 33, "midrange jumper");
 
@@ -30,7 +42,15 @@ console.log(kevinDurant.age);
 console.log(kevinDurant.signatureScoringStyle);
 console.log(kevinDurant.score());
 
-const stephenCurry = new NBAplayer("Stephen Curry", "Golden State Warriors", "6 ft 3 in", 34, "pull up three");
+/*****************************************************************************/
+
+class StephenCurry extends NBAplayer {
+  celebration() {
+    return `${this.name} celebrates with a shoulder shimmy after scoring with a ${this.signatureScoringStyle}`;
+  }
+}
+
+const stephenCurry = new StephenCurry("Stephen Curry", "Golden State Warriors", "6 ft 3 in", 34, "pull up three");
 
 console.log(stephenCurry);
 console.log(stephenCurry.name);
@@ -39,11 +59,22 @@ console.log(stephenCurry.height);
 console.log(stephenCurry.age);
 console.log(stephenCurry.signatureScoringStyle);
 console.log(stephenCurry.score());
+console.log(stephenCurry.celebration());
 
-const jamesHarden = new NBAplayer("James Harden", "Philadelphia 76ers", "6 ft 5 in", 32, "step-back three");
+/*****************************************************************************/
+
+class JamesHarden extends NBAplayer {
+  constructor(name, nickName, team, height, age, signatureScoringStyle) {
+    super(name, team, height, age, signatureScoringStyle);
+    this.nickName = nickName;
+  }
+}
+
+const jamesHarden = new JamesHarden("James Harden", "The Beard", "Philadelphia 76ers", "6 ft 5 in", 32, "step-back three");
 
 console.log(jamesHarden);
 console.log(jamesHarden.name);
+console.log(jamesHarden.nickName);
 console.log(jamesHarden.team);
 console.log(jamesHarden.height);
 console.log(jamesHarden.age);
